68e6fe1f104713044c0f9d65f5c9dac7ce9bde05
Lily Shehadeh <202112445@std-zuj.edu.jo>
2025-02-16 21:06:01 +0300

    Ensure foodRoutes.js is included

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/db.js';
-import foodRouter from './routes/foodRoutes.js';
 import userRouter from './routes/userRoute.js';
 import cartRouter from './routes/cartRoute.js';
 import dotenv from 'dotenv';
 import orderRouter from './routes/orderRoute.js';
 import kitchenRoute from './routes/kitchenRoute.js';
 import deliveryRouter from './routes/deliveryRoutes.js';
-import deliveryRoutes from './routes/deliveryRoutes.js';
+import foodRouter from './routes/foodRoutes.js';
+
 
 dotenv.config();
 
@@ -31,7 +31,6 @@ app.use('/api/cart', cartRouter);
 app.use('/api/order', orderRouter);
 app.use('/api/kitchen-owner', kitchenRoute);
 app.use('/api/delivery', deliveryRouter);
-app.use('/api/delivery', deliveryRoutes);
 
 // sign-up route
 app.post('/auth/signup', async (req, res) => {
